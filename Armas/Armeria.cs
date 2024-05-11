cd86447a478f728017a79cd09ad3c18524ec91b7
jpledezma <jpledezma01@outlook.com>
2024-05-11 02:37:42 -0300

    Se agrega la clase contenedora Armeria

---
+++ Armas/Armeria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armas
+{
+    public class Armeria
+    {
+        private List<ArmaDeFuego> armas;
+
+        public List<ArmaDeFuego> Armas
+        {
+            get { return new List<ArmaDeFuego>(armas); }
+        }
+
+        public Armeria()
+        {
+            this.armas = new List<ArmaDeFuego>();
+        }
+
+        public static List<ArmaDeFuego> operator +(Armeria armeria, ArmaDeFuego arma)
+        {
+            List<ArmaDeFuego> listaActualizada = armeria.Armas;
+            bool estaIncluida = false;
+
+            foreach(ArmaDeFuego a in listaActualizada)
+            {
+                if (arma == a)
+                {
+                    estaIncluida = true;
+                    break;
+                }
+            }
+            if (!estaIncluida)
+            {
+                listaActualizada.Add(arma);
+            }
+            return listaActualizada;
+        }
+
+        public static List<ArmaDeFuego> operator -(Armeria armeria, ArmaDeFuego arma)
+        {
+            List<ArmaDeFuego> listaActualizada = armeria.Armas;
+            if (listaActualizada.Contains(arma)) 
+            {
+                listaActualizada.Remove(arma);
+            }
+            return listaActualizada;
+        }
+    }
+}
