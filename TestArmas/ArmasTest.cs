5beb520878b13592339724ce4a919e4a96e05a73
jpledezma <jpledezma01@outlook.com>
2024-07-02 21:20:11 -0300

    Se agregan test unitarios

---
+++ TestArmas/ArmasTest.cs
@@ -0,0 +1,318 @@
+﻿using Armas;
+using Municion;
+
+namespace TestArmas
+{
+    [TestClass]
+    public class ArmasTest
+    {
+        /// <summary>
+        /// Se comparan 2 armas que solo coinciden en numero de serie y tipo.
+        /// </summary>
+        [TestMethod]
+        public void TestComprobarIgualdad_Ok()
+        {
+            //Arrange
+            PistolaSemiautomatica pistola1;
+            pistola1 = new PistolaSemiautomatica(
+                "pistola_1",
+                "pistola_1",
+                "IGUAL",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                100
+                );
+
+            PistolaSemiautomatica pistola2;
+            pistola2 = new PistolaSemiautomatica(
+                "pistola_2",
+                "pistola_2",
+                "IGUAL",
+                2,
+                EMunicion.PARABELLUM_9x19,
+                new List<EMaterial>() { EMaterial.Polimero },
+                20,
+                200
+                );
+
+
+            bool rta = pistola1 == pistola2;
+
+
+            Assert.IsTrue(rta);
+        }
+
+        /// <summary>
+        /// Se comparan 2 armas que coinciden en todos sus atributos, pero son de diferente tipo.
+        /// </summary>
+        [TestMethod]
+        public void TestComprobarIgualdad_Fallak()
+        {
+            //Arrange
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "IGUAL",
+                "IGUAL",
+                "IGUAL",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                100
+                );
+
+            EscopetaBombeo escopeta;
+            escopeta = new EscopetaBombeo(
+                "IGUAL",
+                "IGUAL",
+                "IGUAL",
+                2,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                100
+                );
+
+
+            bool rta = pistola == escopeta;
+
+
+            Assert.IsFalse(rta);
+        }
+
+        /// <summary>
+        /// Se intenta efectuar un disparo, pero el mismo no es exitoso.<br></br>
+        /// 1er intento: las armas no están cargadas.
+        /// 2do intento: la pistola tiene el seguro activo, la escopeta no está amartillada.
+        /// </summary>
+        [TestMethod]
+        public void TestDisparar_Falla()
+        {
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Polimero },
+                7,
+                1000
+                );
+
+            EscopetaBombeo escopeta;
+            escopeta = new EscopetaBombeo(
+                "escopeta",
+                "escopeta",
+                "escopeta",
+                4.2,
+                EMunicion.SHELL_12g,
+                new List<EMaterial>() { EMaterial.Acero, EMaterial.Madera },
+                5,
+                1000
+                );
+
+            FusilAsalto fusil;
+            fusil = new FusilAsalto(
+                "fusil",
+                "fusil",
+                "fusil",
+                3.6,
+                EMunicion.OTAN_5_56x45,
+                new List<EMaterial>() { EMaterial.Aluminio },
+                30,
+                200,
+                1500
+                );
+
+            bool disparoCargadorVacio = pistola.Disparar() || escopeta.Disparar() || fusil.Disparar();
+
+            pistola.Recargar();
+            escopeta.Recargar();
+
+            bool disparoSeguroActivo = pistola.Disparar() || escopeta.Disparar();
+
+            bool algunDisparoExitoso = disparoCargadorVacio || disparoSeguroActivo;
+
+            Assert.IsFalse( algunDisparoExitoso );
+        }
+
+        /// <summary>
+        /// Se intenta disparar todas las armas. Todas están sin seguro y tienen el cargador lleno.
+        /// </summary>
+        [TestMethod]
+        public void TestDisparar_Ok()
+        {
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Polimero },
+                7,
+                1000
+                );
+
+            EscopetaBombeo escopeta;
+            escopeta = new EscopetaBombeo(
+                "escopeta",
+                "escopeta",
+                "escopeta",
+                4.2,
+                EMunicion.SHELL_12g,
+                new List<EMaterial>() { EMaterial.Acero, EMaterial.Madera },
+                5,
+                1000
+                );
+
+            FusilAsalto fusil;
+            fusil = new FusilAsalto(
+                "fusil",
+                "fusil",
+                "fusil",
+                3.6,
+                EMunicion.OTAN_5_56x45,
+                new List<EMaterial>() { EMaterial.Aluminio },
+                30,
+                200,
+                1500
+                );
+
+            pistola.Recargar();
+            escopeta.Recargar();
+            fusil.Recargar();
+            pistola.CambiarSeguro();
+            escopeta.Amartillar();
+
+            bool todosDisparosExitosos = pistola.Disparar() && escopeta.Disparar() && fusil.Disparar();
+
+            Assert.IsTrue(todosDisparosExitosos);
+        }
+
+        /// <summary>
+        /// Se intenta agregar cartuchos de un calibre diferente a las armas.
+        /// </summary>
+        [TestMethod]
+        public void TestAgregarCartucho_Falla()
+        {
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Polimero },
+                7,
+                1000
+                );
+
+            EscopetaBombeo escopeta;
+            escopeta = new EscopetaBombeo(
+                "escopeta",
+                "escopeta",
+                "escopeta",
+                4.2,
+                EMunicion.SHELL_12g,
+                new List<EMaterial>() { EMaterial.Acero, EMaterial.Madera },
+                5,
+                1000
+                );
+
+            FusilAsalto fusil;
+            fusil = new FusilAsalto(
+                "fusil",
+                "fusil",
+                "fusil",
+                3.6,
+                EMunicion.OTAN_5_56x45,
+                new List<EMaterial>() { EMaterial.Aluminio },
+                30,
+                200,
+                1500
+                );
+
+            List<Cartucho> cartuchos = new List<Cartucho>();
+            cartuchos.Add(new Cartucho(EMunicion.MAGNUM_44));
+            cartuchos.Add(new Cartucho(EMunicion.SOVIET_5_45X39));
+            cartuchos.Add(new Cartucho(EMunicion.WIN_308));
+
+            pistola.Recargar(cartuchos);
+            escopeta.Recargar(cartuchos);
+            fusil.Recargar(cartuchos);
+
+            bool pistolaVacia = pistola.Cargador.CartuchosCargados.Count == 0;
+            bool fusilVacio = fusil.Cargador.CartuchosCargados.Count == 0;
+            bool escopetaVacia = escopeta.CartuchosCargados.Count == 0;
+
+            bool todosVacios = pistolaVacia && fusilVacio && escopetaVacia;
+
+            Assert.IsTrue( todosVacios );
+        }
+
+        /// <summary>
+        /// Se intenta agregar cartuchos del mismo calibre que las armas.
+        /// </summary>
+        [TestMethod]
+        public void TestAgregarCartucho_Ok()
+        {
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Polimero },
+                7,
+                1000
+                );
+
+            EscopetaBombeo escopeta;
+            escopeta = new EscopetaBombeo(
+                "escopeta",
+                "escopeta",
+                "escopeta",
+                4.2,
+                EMunicion.SHELL_12g,
+                new List<EMaterial>() { EMaterial.Acero, EMaterial.Madera },
+                5,
+                1000
+                );
+
+            FusilAsalto fusil;
+            fusil = new FusilAsalto(
+                "fusil",
+                "fusil",
+                "fusil",
+                3.6,
+                EMunicion.OTAN_5_56x45,
+                new List<EMaterial>() { EMaterial.Aluminio },
+                30,
+                200,
+                1500
+                );
+
+            List<Cartucho> cartuchos = new List<Cartucho>();
+            cartuchos.Add(new Cartucho(EMunicion.ACP_45));
+            cartuchos.Add(new Cartucho(EMunicion.SHELL_12g));
+            cartuchos.Add(new Cartucho(EMunicion.OTAN_5_56x45));
+
+            pistola.Recargar(cartuchos);
+            escopeta.Recargar(cartuchos);
+            fusil.Recargar(cartuchos);
+
+            bool pistolaCargada = pistola.Cargador.CartuchosCargados.Count != 0;
+            bool fusilCargado = fusil.Cargador.CartuchosCargados.Count != 0;
+            bool escopetaCargada = escopeta.CartuchosCargados.Count != 0;
+
+            bool todosCargados = pistolaCargada && fusilCargado && escopetaCargada;
+
+            Assert.IsTrue(todosCargados);
+        }
+    }
+}
---
+++ TestArmas/ArmeriaTest.cs
@@ -0,0 +1,181 @@
+using Armas;
+
+namespace TestArmas
+{
+    [TestClass]
+    public class ArmeriaTest
+    {
+        /// <summary>
+        /// Se intentan agregar 2 armas con el mismo numero de serie, pero de distinto tipo.
+        /// </summary>
+        [TestMethod]
+        public void TestAgregar_Ok()
+        {
+            //Arrange
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego> ();
+
+            PistolaSemiautomatica pistola = new PistolaSemiautomatica();
+            FusilAsalto fusil = new FusilAsalto();
+            fusil.NumeroSerie = "IGUAL";
+            pistola.NumeroSerie = "IGUAL";
+
+            //Act
+            armeria += pistola;
+            armeria += fusil;
+
+            bool rta = armeria.Armas.Count == 2;
+            //Assert
+            Assert.IsTrue(rta);
+        }
+
+        /// <summary>
+        /// Se intentan agregar 2 armas del mismo tipo y mismo numero de serie.
+        /// </summary>
+        [TestMethod]
+        public void TeastAgregar_Falla()
+        {
+            //Arrange
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego>();
+
+            PistolaSemiautomatica pistola1 = new PistolaSemiautomatica();
+            PistolaSemiautomatica pistola2 = new PistolaSemiautomatica();
+
+            pistola1.NumeroSerie = "IGUAL";
+            pistola2.NumeroSerie = "IGUAL";
+
+            Action agregar = () => { armeria += pistola2; };
+
+            //Act
+            armeria += pistola1;
+
+            //Assert
+            Assert.ThrowsException<Exception>(agregar);
+        }
+
+        /// <summary>
+        /// Se intenta eliminar un arma presente en la armeria.
+        /// </summary>
+        [TestMethod]
+        public void TestEliminar_Ok()
+        {
+            //Arrange
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego>();
+
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "fabricante",
+                "modelo",
+                "num_serie",
+                1.2,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                137.99
+                );
+
+            //Act
+            int cantidadAnterior;
+            int cantidadSiguiente;
+
+            armeria += pistola;
+            cantidadAnterior = armeria.Armas.Count;
+
+            armeria -= pistola;
+            cantidadSiguiente = armeria.Armas.Count;
+
+            bool rta = cantidadAnterior != cantidadSiguiente && cantidadSiguiente == 0;
+
+            //Assert
+            Assert.IsTrue(rta);
+        }
+
+        /// <summary>
+        /// Se intenta eliminar un arma que no se encuentra en la armeria.
+        /// </summary>
+        [TestMethod]
+        public void TestEliminar_Falla()
+        {
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego>();
+
+            PistolaSemiautomatica pistola = new PistolaSemiautomatica();
+            FusilAsalto fusil = new FusilAsalto();
+
+            pistola.NumeroSerie = "IGUAL";
+            fusil.NumeroSerie = "IGUAL";
+
+            Action eliminar = () => { armeria -= fusil; };
+
+            armeria += pistola;
+
+            Assert.ThrowsException<Exception>(eliminar);
+        }
+
+        /// <summary>
+        /// Se intenta obtener un arma de la armeria accediendo mediante el indexador.
+        /// </summary>
+        [TestMethod]
+        public void TestObtenerIndice_Ok()
+        {
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego>();
+
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1.2,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                137.99
+                );
+
+            FusilAsalto fusil;
+            fusil = new FusilAsalto(
+                "fusil",
+                "fusil",
+                "fusil",
+                4.2,
+                EMunicion.OTAN_5_56x45,
+                new List<EMaterial>() { EMaterial.Aluminio },
+                30,
+                300,
+                1500
+                );
+
+            armeria += pistola;
+            armeria += fusil;
+
+            bool rta = armeria[0] == pistola && armeria[1] == fusil;
+
+            Assert.IsTrue(rta);
+        }
+
+        /// <summary>
+        /// Se intenta acceder a un arma en la armeria con un indice fuera del rango de la misma.
+        /// </summary>
+        [TestMethod]
+        public void TestObtenerIndice_Falla()
+        {
+            Armeria<ArmaDeFuego> armeria = new Armeria<ArmaDeFuego>();
+
+            PistolaSemiautomatica pistola;
+            pistola = new PistolaSemiautomatica(
+                "pistola",
+                "pistola",
+                "pistola",
+                1.2,
+                EMunicion.ACP_45,
+                new List<EMaterial>() { EMaterial.Acero },
+                10,
+                137.99
+                );
+
+            armeria += pistola;
+            Action accion = () => { ArmaDeFuego arma = armeria[1]; };
+
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(accion);
+        }
+    }
+}
